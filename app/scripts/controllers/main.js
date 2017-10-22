99e00e53774f7aa2e495bf4c795e699557fa3913
Priyanka Gangireddy <priyanka.sfdc8@gmail.com>
2017-10-22 16:39:42 -0400

    setup is done

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quixamGlobalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quixamGlobalApp
+ */
+angular.module('quixamGlobalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
